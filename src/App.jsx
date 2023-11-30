379d40b846033e5820ed4f609d5b085ccac7e11f
ThomassMathisen <thomasmathisen@hotmail.com>
2023-11-30 14:20:37 -0300

    Setup global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import "./app.scss";
+
 function App() {
-  return <div className="div">Hello World</div>;
+  return (
+    <div className="div">
+      <section>Test</section>
+      <section>Test3</section>
+      <section>Test2</section>
+      <section>Portfolio</section>
+      <section>Contact</section>
+    </div>
+  );
 }
 
 export default App;

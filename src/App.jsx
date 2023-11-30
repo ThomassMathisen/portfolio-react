8039b817ebdb3bb6b15fbd174460120dd20d1b01
ThomassMathisen <thomasmathisen@hotmail.com>
2023-11-30 19:40:06 -0300

    Creates sections with parallax motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 function App() {
   return (
@@ -9,9 +10,13 @@ function App() {
         <Navbar />
         <Hero />
       </section>
-      <section>Test3</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
       <section>Test2</section>
-      <section>Portfolio</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Contact</section>
     </div>
   );

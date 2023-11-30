937d9f1a2e842a57246fa050a7f7b27c0d52d952
ThomassMathisen <thomasmathisen@hotmail.com>
2023-11-30 16:41:00 -0300

    Create navbar and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="div">
-      <section>Test</section>
+      <section id="Homepage">
+        <Navbar />
+      </section>
       <section>Test3</section>
       <section>Test2</section>
       <section>Portfolio</section>
